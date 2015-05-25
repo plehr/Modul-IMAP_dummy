d949a48e92c706e8fec7af1859fd7ca4e0e2dd90
P Lehr <spam@plehr.de>
2015-05-25 16:04:59 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+
+	
+import java.util.List;
+
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		start();
+	}
+
+
+	public static void start() {
+		System.out.println("test");
+		IMAP mail = new IMAP();
+		mail.start("imap.gmx.net", "pascallehr@gmx.de", "12345");
+		
+		List<String> von = mail.getAusgabearray_von();
+		List<String> datum = mail.getAusgabearray_datum();
+		List<String> betreff = mail.getAusgabearray_betreff();
+
+		
+		int AnzahlDS = betreff.size();
+		if (AnzahlDS == 0){
+			System.out.println("Keine neuen Mails");
+		} else {
+		System.out.println("--------------- AUSGABE DER UNGELESENEN MAILS ---------------");
+		AnzahlDS--;
+		while (AnzahlDS != -1) {
+			System.out.println("Nummer: " + (AnzahlDS+1));
+			System.out.println("Von: " + von.get(AnzahlDS));
+			System.out.println("Datum: " + datum.get(AnzahlDS));
+			System.out.println("Betreff: " + betreff.get(AnzahlDS));
+			System.out.println("");
+			AnzahlDS--;
+		}
+		
+		}System.exit(0);
+	}
+}
